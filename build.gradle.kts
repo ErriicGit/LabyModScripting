b7a9c7897b09fcc060099dbec9badc56f052bfbb
Erriic <121909544+ErriicGit@users.noreply.github.com>
2024-04-23 00:47:43 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ labyMod {
         author = "Erriic"
         description = "Scripting in Labymod"
         minecraftVersion = "*"
-        version = System.getenv().getOrDefault("VERSION", "1.0.0")
+        version = System.getenv().getOrDefault("VERSION", "1.0.1")
     }
 
     minecraft {
